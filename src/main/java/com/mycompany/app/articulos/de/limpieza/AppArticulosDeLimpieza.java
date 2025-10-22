705f3fb88415b935d9130a532b6131a18fb54a11
Omar Mayca <omartauro21@gmail.com>
2025-10-21 20:32:47 -0500

    Versión inicial del proyecto Artículos de Limpieza

---
+++ src/main/java/com/mycompany/app/articulos/de/limpieza/AppArticulosDeLimpieza.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.app.articulos.de.limpieza;
+
+/**
+ *
+ * @author OMAR
+ */
+public class AppArticulosDeLimpieza {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
