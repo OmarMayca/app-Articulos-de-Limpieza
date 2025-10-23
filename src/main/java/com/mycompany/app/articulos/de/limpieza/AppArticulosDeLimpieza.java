becfd2c665ccf9b2c0488bec61a40faf6ba1fa22
oyoyya61-ship-it <oyoyya61@gmail.com>
2025-10-22 19:26:45 -0500

    Update AppArticulosDeLimpieza.java

--- src/main/java/com/mycompany/app/articulos/de/limpieza/AppArticulosDeLimpieza.java
+++ src/main/java/com/mycompany/app/articulos/de/limpieza/AppArticulosDeLimpieza.java
@@ -13,5 +13,6 @@ public class AppArticulosDeLimpieza {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("hola amigos como estan amigos traviesos, omar MAS Z, 04, peru es clave, gaaaaaaaaaaa, xd, profe mantari mi presi");
-    }
+        System.out.println("lo logré");
+	}
 }
