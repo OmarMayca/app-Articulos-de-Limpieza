02fe4a4971d882d3dfc9fe94abeba62606df5792
Piero Matias Montañez Bellido <pm049849@gmail.com>
2025-10-23 16:17:07 -0500

    se agregaron las sub clases

---
+++ src/main/java/productos/papeles/PapelParacas.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package productos.papeles;
+
+public class PapelParacas extends Papel {
+    public PapelParacas() {
+        super("Papel Paracas", 6.00, 100);
+    }
+}
